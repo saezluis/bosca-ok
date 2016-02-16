53da47d5c96ea078d828d4fcf36f7e2e3ab35250
saezluis <lsaez@pm.cl>
2016-02-16 19:52:50 -0300

    Cupon Climastar

--- index.php
+++ index.php
@@ -24,8 +24,11 @@ if(isset($_POST['cotizar_prod'])) {
 <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
 <link rel="stylesheet" href="owl-carousel/owl.theme.css">
 <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="main2.js"></script>
 <script src="sass/tema/js/scripts.js"></script>
 <script src="owl-carousel/owl.carousel.min.js"></script>
+
+<link rel="stylesheet" href="main.css">
 
 <script type="text/javascript">
 
@@ -265,5 +268,37 @@ function comentario(){
       </div>
     </div>
   </footer>  
+  
+  <div id="boxes">
+	  <div style="display: none; position:relative;" id="dialog" class="window centrado-porcentual"> 
+			<!--
+			<h1>Súbete al programa donde todos crecemos, siguiendo estas indicaciones.</h1>
+				<div id="lorem">
+					<ul class="lista-pop">
+						<li>Ingresa tu rut sin puntos ni dígito verificador.</li>
+						<li>*ingresa tu clave (tres últimos dígitos de tu rut)</li>
+					</ul>
+				</div>
+				-->
+				<div class="grupo">
+				  <div class="imgCupon">
+					<div class="caja base-100 tablet-50">
+					  <div class="pro-l"><img src="img/productos_light.png" alt=""></div>
+					</div>
+					<div class="caja base-100 tablet-50">
+					  <div class="logo-light-L"><img src="img/logo-light.png" alt=""></div>
+					  <div class="texti-L"><img src="img/texto-light.png" alt=""><a href="img/cupon.jpg" target="_blank" class="getCupon">¡Lo quiero!</a></div>
+					</div>
+				  </div>
+				</div>
+				
+				
+				<!--
+				<a href="#" class="close close-imagenes">X</a>
+				-->
+		</div>
+				<div style="width: 1478px; font-size: 32pt; color:white; height: 602px; display: none; opacity: 0.8;" id="mask"></div>
+	</div>
+  
 </body>
 </html>
